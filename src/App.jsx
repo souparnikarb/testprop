27f2976932f3eb5a511bd672c403d2371716721f
souparnikarb <souparnikarb714@gmail.com>
2025-10-08 09:32:15 +0530

    adding a button  souparnikarb

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,9 @@ function handleClick2(){
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <button onClick={handleClick}>souparnika</button>
+        <button onClick={handleClick1}>souparnika</button>
+        <button onClick={handleClick3}>souparnikarb</button>
+        <button onClick={handleClick2}>sherin</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
